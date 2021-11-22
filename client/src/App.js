a7e662a7935052c414d63ac27417a6ac3ba7aaef
RiceinPot <jdeng2019@gmail.com>
2021-11-22 18:23:37 -0500

    initial commit to main

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import axios from "axios";
 import React, {useState, useEffect} from "react";
 import './App.css';
@@ -7,24 +6,50 @@ function App() {
 
   const nodeServer = process.env.SERVER_URL || "http://localhost:5000";
 
-  const [tickets, setTickets] = useState([]);
+  const [tickets, setTickets] = useState([]);  //TODO: use state inifnite loop 
+  const [pages, setPages] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
 
   })
 
   const getTickets = () => {
-    axios.get(nodeServer + '/').then((res) => {
-      console.log(res);
-    })
+    setLoading(true);
+    (async() => {
+      try {
+        var response = await axios({
+          url: '/',
+          baseURL: nodeServer
+        })
+        console.log(response);
+        setTickets([...response.data]);
+        console.log(tickets);
+        var newPages = [];
+        while(tickets.length > 25){
+          var currPage = tickets.splice(0, 25);
+          newPages.push(currPage);
+        }
+        newPages.push(tickets);
+        setPages([...newPages]);
+        console.log(pages);
+        setLoading(false);
+      } catch(error) {
+        console.log('oops');
+      } 
+    })()
+    
   }
+
+
 
 
   return (
     <div className="App">
-      <button onClick={getTickets()}> Click me </button>
+      <button onClick={getTickets}> Click me </button>
+      {loading ? 'Please wait' : ''}
     </div>
-  );
+  )
 }
 
 export default App;
--- server.js
+++ server.js
@@ -7,27 +7,26 @@ const axios = require('axios');
 const API_URL = "https://zccjdeng88.zendesk.com/api/v2";
 const API_USER = require("./user.js");
 
-console.log(API_USER);
-
 app.use(cors());
 
 app.get('/', function(req, res){
     (async () => {
         try {
-            const response = await axios({
-               url: '/tickets.json',
-               baseURL: API_URL,
-               headers: {
-                   "Content-Type": "application/json",
-                   "Authorization": "Basic " + API_USER
-               }
-            })
-            console.log(response);
-            res.send("hello world");
+            console.log("tried");
+            var response = await axios({
+                url: "/tickets.json",
+                baseURL: API_URL,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Basic " + API_USER
+                    }
+                })
+            console.log(response.data.tickets);
+            res.send(response.data.tickets);
         } catch (error){
-            console.log(error);
+            res.send(null);
         }
-    })
+    })()
 })
 
 
@@ -36,4 +35,3 @@ app.listen(5000, () => {
     console.log("Listening on port 5000");
 })
 
-//TODO: fix api request. Working in postman but not in app
